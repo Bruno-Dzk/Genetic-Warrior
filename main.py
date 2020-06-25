d961f9cb473dbc361086cb53ff566636179a4bec
Bruno-Dzk <dzikowski.bruno@gmail.com>
2020-06-25 12:31:35 +0200

    Final final fixes. Corrected score checks once more.

--- main.py
+++ main.py
@@ -120,14 +120,14 @@ class Game:
         If on the left side player two gets one point.
         """
         for ball in self.ball_list:
-            if ball.position.x > SCREEN_WIDTH - ball.radius:
+            if ball.position.x > SCREEN_WIDTH + ball.radius:
                 self.score[PLAYER_ONE] += 1
                 self.player_one_scoreboard.update_score(self.score[PLAYER_ONE])
                 ball.cleanup_needed = True
                 self.create_ball((SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2),
                                  (SCREEN_WIDTH, SCREEN_HEIGHT/2),
                                  Colors.BALL_COLOR)
-            if ball.position.x < ball.radius:
+            if ball.position.x < -ball.radius:
                 self.score[PLAYER_TWO] += 1
                 self.player_two_scoreboard.update_score(self.score[PLAYER_TWO])
                 ball.cleanup_needed = True
